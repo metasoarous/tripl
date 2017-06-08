0dec67631b9d5ceaee437c9ce195c3a93cb7dc94
Christopher Small <csmall@fhcrc.org>
2017-06-08 16:29:07 -0700

    outputting json per nest level (no-agg)

--- tripl/nestly.py
+++ tripl/nestly.py
@@ -2,15 +2,29 @@
 import os
 import json
 import copy
-#import pprint as pp
+import uuid
+import pprint as pp
+pp #lint
 
 import SCons.Node
+
+
+def json_encoder_default(obj):
+    if isinstance(obj, (SCons.Node.FS.Entry, SCons.Node.FS.File, uuid.UUID)):
+        return str(obj)
+    elif isinstance(obj, (SCons.Node.NodeList, set)):
+        return list(obj)
+    else:
+        # warn?
+        return list(obj)
+
 
 def _create_metadata_file(source, target, env):
     target = str(target[0])
     #pp.pprint(env['metadata_dict'])
+    print("creating metadata file")
     with open(target, 'w') as fp:
-        json.dump([env['metadata_dict']], fp, indent=4, default=list) #, cls=env['encoder_cls'])
+        json.dump([env['metadata_dict']], fp, indent=4, default=json_encoder_default) #, cls=env['encoder_cls'])
 
 def _has_namespace(name):
     return len(name.split(':')) > 1
@@ -33,9 +47,16 @@ class NestWrap(object):
             'metadata': metadata,
             'id_attrs': id_attrs or [],
             'parent_nest': None,
+            'aggregate': [],
+            'child_nests': [],
             'targets': set()
             }}
         self.targets = {}
+
+        @self.add_target(name=namespace + '.db:ident')
+        def _ident_fn(outdir, c):
+            val_uuid = uuid.uuid1()
+            return val_uuid
 
     # These are more or less done, and just wrap other things below
     # =============================================================
@@ -63,17 +84,35 @@ class NestWrap(object):
         namespace = namespace or name
         namespace = (self.base_namespace + "." if self.base_namespace else "") + namespace
         # Do something with id_attrs and meta...
-        self.nest_levels[name] = {
-                'name': name,
-                'namespace': namespace,
-                'metadata': metadata,
-                'label_func': kw_args.get('label_func'),
-                # We want to know all parent id_attrs as well when merging down
-                'id_attrs': (id_attrs or []) + self.nest_levels[self.current_nest]['id_attrs'],
-                'parent_nest': self.current_nest,
-                'targets': set()}
+        parent_level = self.nest_levels[self.current_nest]
+        nest_level = {
+            'name': name,
+            'namespace': namespace,
+            'metadata': metadata,
+            'label_func': kw_args.get('label_func', lambda x: x),
+            # We want to know all parent id_attrs as well when merging down
+            'id_attrs': (id_attrs or []) + self.nest_levels[self.current_nest]['id_attrs'],
+            'ident_attr': namespace + '.db:ident',
+            'parent_nest': self.current_nest,
+            'child_nests': [],
+            'targets': set()}
+        self.nest_levels[name] = nest_level
+
+        self.nest_levels[self.current_nest]['child_nests'].append(name)
         self.current_nest = name
-        return self.scons_wrap.add(name, nestable, **kw_args)
+
+
+        return_val = self.scons_wrap.add(name, nestable, **kw_args)
+        @self.add_target(name=namespace + '.db:ident')
+        def _ident_fn(outdir, c):
+            parent_ident = c.get(parent_level.get('ident_attr'))
+            if not parent_ident:
+                parent_ident = uuid.uuid3(uuid.NAMESPACE_URL, parent_level['namespace'])
+            attr_uuid = uuid.uuid3(parent_ident, name)
+            val_uuid = uuid.uuid3(attr_uuid, nest_level['label_func'](c[name]))
+            return val_uuid
+
+        return return_val
 
     # add namespace arg here?
     def add_target(self, name=None, metadata=None, omit_metadata=False):
@@ -92,10 +131,17 @@ class NestWrap(object):
             return f_
         return deco
 
-    def pop(self, name=None, env=None, file_name='metadata.json'):
+    def _pop(self, env=None, file_name='metadata.json'):
         self.dump_metadata(env, file_name=file_name)
-        self.scons_wrap.pop(name)
-        self.current_nest = self.nest_levels[name or self.current_nest]['parent_nest']
+        self.scons_wrap.pop()
+        self.current_nest = self.nest_levels[self.current_nest]['parent_nest']
+
+
+    def pop(self, name=None, env=None, file_name='metadata.json'):
+        while name and name != self.current_nest:
+            print("poppping", self.current_nest)
+            self._pop(env=env, file_name=file_name)
+        self._pop(env=env, file_name=file_name)
 
     # Some implementation details...
 
@@ -146,6 +192,7 @@ class NestWrap(object):
 
 
     def _translated_metadata_dict(self, c, base_nest_level=None):
+        #orig_base_nest_level = base_nest_level
         base_nest_level = base_nest_level or self.current_nest
         nest_level = self.nest_levels[base_nest_level]
         nest_id = nest_level['namespace'] + ':id'
@@ -177,9 +224,21 @@ class NestWrap(object):
                 v = self._translate_target(c, a, v)
                 d[self._namespaced(a, base_nest_level=base_nest_level)] = v
 
+        # We've decided we're not doing this; We'll merge everything together at the end.
+        #if not orig_base_nest_level:
+            #for child_nest in nest_level['child_nests']:
+                #child_level = self.nest_levels[child_nest]
+                #d[self._namespaced('_' + self.current_nest, child_nest)] = child_level['aggregate']
+
+        d['db:ident'] = c[nest_level['namespace'] + '.db:ident']
+
         parent_nest_level = nest_level['parent_nest']
         if parent_nest_level:
-            d[self._namespaced(parent_nest_level)] = self._translated_metadata_dict(c, base_nest_level=parent_nest_level)
+            # For full, do this; maybe on flag or something
+            #d[self._namespaced(parent_nest_level)] = self._translated_metadata_dict(c, base_nest_level=parent_nest_level)
+            parent_nest = self.nest_levels[parent_nest_level]
+            parent_ident_name = parent_nest['namespace'] + '.db:ident'
+            d[self._namespaced(parent_nest_level)] = {'db:ident': c[parent_ident_name]}
         # Should also be serializing a secondary representation of the 
         return d
 
@@ -188,17 +247,21 @@ class NestWrap(object):
 
     def dump_metadata(self, env=None, target_name='_metadata', file_name='metadata.json'):
         env = env or self.scons_wrap.alias_environment
-        @self.add_target(name=target_name)
         # Using `_` by default because 
+        @self.add_target(name=target_name)
         def _metadata(outdir, c):
+            translated_metadata = self._translated_metadata_dict(c)
+            #self.nest_levels[self.current_nest]['aggregate'].append(translated_metadata)
+            #print "Creating metadata tgt"
             main_tgt = env.Command(os.path.join(outdir, file_name),
+                               # Should actually make this depend on all the other files? as a flag?
                                [],
                                action=_create_metadata_file,
-                               metadata_dict=self._translated_metadata_dict(c))
+                               metadata_dict=translated_metadata)
                                #encoder_cls=encoder_cls)
             env.AlwaysBuild(main_tgt)
-            return main_tgt
             # TODO Add another step that ingests data marked for ingest
+            return main_tgt
 
     # Eventually; need to reconcile with nesting as implicit aggregation for some cases
     #def add_aggregate(self, name, data_fac):
