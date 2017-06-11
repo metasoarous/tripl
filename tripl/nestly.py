f20f04f79a9fa1c4dafcbd7b6c11d734be3a107f
Christopher Small <csmall@fhcrc.org>
2017-06-11 14:46:16 -0700

    stagewise ingestion and aggregation of scons tgt data

--- tripl/nestly.py
+++ tripl/nestly.py
@@ -3,28 +3,100 @@ import os
 import json
 import copy
 import uuid
+import tripl
 import pprint as pp
 pp #lint
 
 import SCons.Node
 
+from Bio import SeqIO
+from Bio import Phylo
 
-def json_encoder_default(obj):
-    if isinstance(obj, (SCons.Node.FS.Entry, SCons.Node.FS.File, uuid.UUID)):
-        return str(obj)
-    elif isinstance(obj, (SCons.Node.NodeList, set)):
-        return list(obj)
-    else:
-        # warn?
-        return list(obj)
+#import tripl
+
+
+def json_encoder_default(path_depth=0):
+    def _json_encoder_default(obj):
+        if isinstance(obj, (SCons.Node.FS.Entry, SCons.Node.FS.File, uuid.UUID)):
+            return os.path.join(str(obj).split('/')[path_depth:])
+        elif isinstance(obj, (SCons.Node.NodeList, set)):
+            return list(obj)
+        else:
+            # warn?
+            return str(obj)
+    return _json_encoder_default
 
 
 def _create_metadata_file(source, target, env):
     target = str(target[0])
     #pp.pprint(env['metadata_dict'])
-    print("creating metadata file")
+    json_sources = [json.load(file(str(f))) for f in source if str(f).split('.')[-1] == 'json']
+    doc = env['metadata_dict']
+    for json_source in json_sources:
+        doc.update(json_source)
+    # TODO; handle other ingested files, maybe just reading in file contents for now, eventually fully tupling
+    # them
     with open(target, 'w') as fp:
-        json.dump([env['metadata_dict']], fp, indent=4, default=json_encoder_default) #, cls=env['encoder_cls'])
+        json.dump([doc], fp, indent=4, default=json_encoder_default()) #, cls=env['encoder_cls'])
+
+
+def ingest_seqs(filename):
+    seqs = SeqIO.parse(filename, 'fasta')
+    return {'bio.seq:set': [{'bio.seq:id': seq.id, 'bio.seq:seq': str(seq.seq)} for seq in seqs]}
+
+
+
+def ingest_newick(filename):
+    trees = list(Phylo.parse(filename, 'newick'))
+    def ingest_clade(clade):
+        d = clade.__dict__
+        children = d.pop('clades')
+        return tripl.namespaced('bio.phylo.clade',
+                clades=map(ingest_clade, children),
+                **{k: v for k, v in d.items() if v})
+    def ingest_tree(tree):
+        return {'bio.phylo.tree:root': ingest_clade(tree.root)}
+    if len(trees) > 1:
+        return {'bio.phylo.tree:set': map(ingest_tree, trees)}
+    else:
+        return ingest_tree(trees[0])
+
+
+def _ingest_metadata_files(source, target, env):
+    target = str(target[0])
+    # First we take care of the 
+    ingest_docs = (json.load(file(str(src))) for src in source if str(src).split('.')[-1] == 'json')
+    ingest_docs = [(doc[0] if isinstance(doc, list) else doc) for doc in ingest_docs]
+    doc = env['metadata_dict']
+    if isinstance(doc, list):
+        doc = doc[0]
+    for ingest_doc in ingest_docs:
+        doc.update(ingest_doc)
+    doc['tripl:aggregate'] = doc.get('tripl:aggregate', [])
+    for other_file in (str(src) for src in source if str(src).split('.')[-1] != 'json'):
+        v = {'db:ident': env['file_idents'][other_file],
+             'tripl.file:contents': file(other_file).read()}
+        fmt = other_file.split('.')[-1]
+        if fmt in {'fasta', 'fa'}:
+            v.update(ingest_seqs(other_file))
+        #if fmt in {'newick', 'nw', 'nwk'}:
+            #v.update(ingest_newick(other_file))
+        doc['tripl:aggregate'].append(v)
+    with open(target, 'w') as fp:
+        json.dump([doc], fp, indent=4, default=json_encoder_default())
+
+def _ingest_aggregates(source, target, env):
+    target = str(target[0])
+    ingest_docs = (json.load(file(str(src))) for src in source)
+    ingest_docs = [(doc[0] if isinstance(doc, list) else doc) for doc in ingest_docs]
+    doc = env['metadata_dict']
+    if isinstance(doc, list):
+        doc = doc[0]
+    doc['tripl:aggregate'] = doc.get('tripl:aggregate', [])
+    doc['tripl:aggregate'] += ingest_docs
+    with open(target, 'w') as fp:
+        json.dump([doc], fp, indent=4, default=json_encoder_default())
+
 
 def _has_namespace(name):
     return len(name.split(':')) > 1
@@ -47,11 +119,18 @@ class NestWrap(object):
             'metadata': metadata,
             'id_attrs': id_attrs or [],
             'parent_nest': None,
-            'aggregate': [],
+            'ident_attr': namespace + '.db:ident',
+            'aggregate_attrs': [],
             'child_nests': [],
             'targets': set()
             }}
         self.targets = {}
+
+        self.file_idents = {}
+
+        #@self.add_target()
+        #def _base_aggregate(outdir, c):
+            #return []
 
         @self.add_target(name=namespace + '.db:ident')
         def _ident_fn(outdir, c):
@@ -78,13 +157,15 @@ class NestWrap(object):
     # This is the real meat of things as an interface:
     # ================================================
 
-    def add(self, name, nestable, namespace=None, metadata=None, id_attrs=None, **kw_args):
+    def add(self, name, nestable, namespace=None, metadata=None, full_dump=False, id_attrs=None, **kw_args):
         """Calls out to the scons_wrap add method, and contextually asserts the given id_attrs. Effectively
         defines a context in which all traversed data gets referenced, and upon which identity is asserted."""
+        self.base_name = name
         namespace = namespace or name
         namespace = (self.base_namespace + "." if self.base_namespace else "") + namespace
         # Do something with id_attrs and meta...
         parent_level = self.nest_levels[self.current_nest]
+        aggregate_attr = '_' + namespace + ':aggregate'
         nest_level = {
             'name': name,
             'namespace': namespace,
@@ -93,8 +174,11 @@ class NestWrap(object):
             # We want to know all parent id_attrs as well when merging down
             'id_attrs': (id_attrs or []) + self.nest_levels[self.current_nest]['id_attrs'],
             'ident_attr': namespace + '.db:ident',
+            'aggregate_attr': aggregate_attr,
+            'aggregate_attrs': parent_level['aggregate_attrs'] + [aggregate_attr] if full_dump else parent_level['aggregate_attrs'],
             'parent_nest': self.current_nest,
             'child_nests': [],
+            'full_dump': full_dump,
             'targets': set()}
         self.nest_levels[name] = nest_level
 
@@ -103,7 +187,7 @@ class NestWrap(object):
 
 
         return_val = self.scons_wrap.add(name, nestable, **kw_args)
-        @self.add_target(name=namespace + '.db:ident')
+        @self.add_target(name=nest_level['ident_attr'])
         def _ident_fn(outdir, c):
             parent_ident = c.get(parent_level.get('ident_attr'))
             if not parent_ident:
@@ -112,36 +196,47 @@ class NestWrap(object):
             val_uuid = uuid.uuid3(attr_uuid, nest_level['label_func'](c[name]))
             return val_uuid
 
+        if full_dump:
+            @self.add_target(name=aggregate_attr)
+            def _aggregate_fn(outdir, c):
+                return []
+
+            # Each level should be able to have it's own dump eventually, but for now, it's assumed there will
+            # only be one, and this will be the relative directory used for all paths written out at this level
+            @self.add_target(name='_output_wrt')
+            def outdir_fn(outdir, c):
+                return outdir
+
+
         return return_val
 
     # add namespace arg here?
-    def add_target(self, name=None, metadata=None, omit_metadata=False):
+    def add_target(self, name=None, metadata=None, omit_metadata=False, ingest=False):
         def deco(f):
             real_name = f.__name__ or name
             real_name = name or f.__name__
             self.targets[real_name] = {
                     'name': real_name,
-                    #'namespaced_name': self.nest_levels[self.current_nest]['namespace'] + ':' + name,
                     'doc': f.__doc__,
                     'metadata': metadata,
                     'nest': self.current_nest,
+                    'ingest': ingest,
                     'omit_metadata': omit_metadata or real_name[0:1] == '_'}
             self.nest_levels[self.current_nest]['targets'].add(real_name)
             f_ = self.scons_wrap.add_target(real_name)(f)
             return f_
         return deco
 
-    def _pop(self, env=None, file_name='metadata.json'):
-        self.dump_metadata(env, file_name=file_name)
+    def _pop(self, env=None, file_name='metadata.json', full_dump=False):
+        self.dump_metadata(env, file_name=file_name, full_dump=full_dump)
         self.scons_wrap.pop()
         self.current_nest = self.nest_levels[self.current_nest]['parent_nest']
 
 
-    def pop(self, name=None, env=None, file_name='metadata.json'):
-        while name and name != self.current_nest:
-            print("poppping", self.current_nest)
-            self._pop(env=env, file_name=file_name)
-        self._pop(env=env, file_name=file_name)
+    def pop(self, name=None, env=None, file_name='metadata.json', full_dump=False):
+            while name and name != self.current_nest:
+                self._pop(env=env, file_name=file_name)
+            self._pop(env=env, file_name=file_name, full_dump=full_dump)
 
     # Some implementation details...
 
@@ -172,26 +267,34 @@ class NestWrap(object):
     def _translate_target(self, c, a, v):
         target = self.targets[a]
         metadata = target['metadata'](c, v) if callable(target['metadata']) else (target['metadata'] or {})
+        def relative_path(p):
+            p = str(p)
+            if p[0:1] != '/':
+                return os.path.relpath(p, c['_output_wrt'])
+            else:
+                return p
         if isinstance(v, dict):
             v = copy.deepcopy(v)
         elif isinstance(v, list) or isinstance(v, SCons.Node.NodeList):
             return [self._translate_target(c, a, v_) for v_ in v]
         elif isinstance(v, SCons.Node.FS.Entry) or isinstance(v, SCons.Node.FS.File):
             # Can add more metadata here as needed
-            v = {'tripl.file:path': str(v),
+            ident = uuid.uuid3(c[self.nest_levels[self.current_nest]['ident_attr']], str(v))
+            self.file_idents[str(v)] = ident
+            v = {'db:ident': ident,
+                 'tripl.file:path': relative_path(v),
                  # This will be super cool...
-                 'tripl.file:sources': [{'tripl.file:path': str(p)} for p in v.sources]}
-            if target['doc']:
-                v['tripl:doc'] = target['doc']
+                 'tripl.file:sources': [{'tripl.file:path': relative_path(p)} for p in v.sources]}
         if isinstance(v, dict):
             v.update(metadata)
         elif metadata:
-            v = metadata['tripl.nestly.target:value'] = v
+            metadata['tripl.nestly.target:value'] = v
+            v = metadata
         # TODO namespace all keywords
         return v
 
 
-    def _translated_metadata_dict(self, c, base_nest_level=None):
+    def _translated_metadata_dict(self, c, base_nest_level=None, full_dump=False):
         #orig_base_nest_level = base_nest_level
         base_nest_level = base_nest_level or self.current_nest
         nest_level = self.nest_levels[base_nest_level]
@@ -224,42 +327,75 @@ class NestWrap(object):
                 v = self._translate_target(c, a, v)
                 d[self._namespaced(a, base_nest_level=base_nest_level)] = v
 
-        # We've decided we're not doing this; We'll merge everything together at the end.
-        #if not orig_base_nest_level:
-            #for child_nest in nest_level['child_nests']:
-                #child_level = self.nest_levels[child_nest]
-                #d[self._namespaced('_' + self.current_nest, child_nest)] = child_level['aggregate']
+        d['db:ident'] = c[nest_level['ident_attr']]
+        d['tripl:type'] = nest_level['namespace']
 
-        d['db:ident'] = c[nest_level['namespace'] + '.db:ident']
+        def add_parent_attrs(parent_nest_level):
+            if parent_nest_level:
+                parent_nest = self.nest_levels[parent_nest_level]
+                parent_ident_name = parent_nest['ident_attr']
+                if nest_level['full_dump']:
+                    d[self._namespaced(parent_nest_level)] = self._translated_metadata_dict(c, base_nest_level=parent_nest_level)
+                else:
+                    d[self._namespaced(parent_nest_level)] = {'db:ident': c[parent_ident_name]}
+                if parent_nest['parent_nest']:
+                    add_parent_attrs(parent_nest['parent_nest'])
 
-        parent_nest_level = nest_level['parent_nest']
-        if parent_nest_level:
-            # For full, do this; maybe on flag or something
-            #d[self._namespaced(parent_nest_level)] = self._translated_metadata_dict(c, base_nest_level=parent_nest_level)
-            parent_nest = self.nest_levels[parent_nest_level]
-            parent_ident_name = parent_nest['namespace'] + '.db:ident'
-            d[self._namespaced(parent_nest_level)] = {'db:ident': c[parent_ident_name]}
+        add_parent_attrs(nest_level['parent_nest'])
         # Should also be serializing a secondary representation of the 
         return d
+
 
     # All to accomodate this; the export of the control dictionary as a tripl json file
     # TODO Need to add ingest steps for things flagged ingest=True
 
-    def dump_metadata(self, env=None, target_name='_metadata', file_name='metadata.json'):
+    def dump_metadata(self, env=None, target_name='_metadata', file_name='metadata.json', full_dump=True):
         env = env or self.scons_wrap.alias_environment
+        current_nest = self.nest_levels[self.current_nest]
+        parent_nest = self.nest_levels[current_nest['parent_nest']]
         # Using `_` by default because 
         @self.add_target(name=target_name)
         def _metadata(outdir, c):
-            translated_metadata = self._translated_metadata_dict(c)
-            #self.nest_levels[self.current_nest]['aggregate'].append(translated_metadata)
-            #print "Creating metadata tgt"
-            main_tgt = env.Command(os.path.join(outdir, file_name),
+            translated_metadata = self._translated_metadata_dict(c, full_dump=full_dump)
+            #for attr in parent_nest['aggregate_attrs']:
+                #print "Finding attr in parent nest from main loop", attr
+                #c[attr].append(translated_metadata)
+            ingest_attrs = [attr for attr in current_nest['targets'] if self.targets[attr]['ingest']]
+            ingest_tgts = [c[attr] for attr in ingest_attrs]
+            # First we go through and write out the preingested json (do we really need this?)
+            pre_ingest_file_name = ".preingest." + file_name
+            pre_ingest_tgt = env.Command(os.path.join(outdir, pre_ingest_file_name),
                                # Should actually make this depend on all the other files? as a flag?
-                               [],
+                               ingest_tgts,
                                action=_create_metadata_file,
                                metadata_dict=translated_metadata)
-                               #encoder_cls=encoder_cls)
-            env.AlwaysBuild(main_tgt)
+            env.AlwaysBuild(pre_ingest_tgt)
+            # Then we go through and ingest metadata files or other data (fasta, newick, etc)
+            pre_agg_file_name = ".preagg." + file_name if current_nest['full_dump'] else file_name
+            pre_agg_tgt = env.Command(os.path.join(outdir, pre_agg_file_name),
+                               # Should actually make this depend on all the other files? as a flag?
+                               [pre_ingest_tgt] + ingest_tgts,
+                               action=_ingest_metadata_files,
+                               metadata_dict=translated_metadata,
+                               file_idents=self.file_idents)
+            env.Depends(pre_agg_tgt, pre_ingest_tgt)
+            env.AlwaysBuild(pre_agg_tgt)
+            # If we're doing a full dump then we also want to aggregate over all the other data that's been
+            # built
+            if current_nest['full_dump']:
+                #env.Depends(main_tgt, c[current_nest['aggregate_attr']])
+                main_tgt = env.Command(os.path.join(outdir, file_name),
+                                   # Should actually make this depend on all the other files? as a flag?
+                                   c[current_nest['aggregate_attr']],
+                                   action=_ingest_aggregates,
+                                   metadata_dict=translated_metadata)
+                env.Depends(main_tgt, pre_agg_tgt)
+                env.AlwaysBuild(main_tgt)
+            else:
+                main_tgt = pre_agg_tgt
+
+            for attr in parent_nest['aggregate_attrs']:
+                c[attr].append(main_tgt)
             # TODO Add another step that ingests data marked for ingest
             return main_tgt
 
